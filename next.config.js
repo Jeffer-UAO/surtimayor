5314f81a38272fb11cf3c516c245c62873738827
Jeffer-UAO <jeffer443@gmail.com>
2023-05-13 17:58:22 -0500

    prueba pwa

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
-// const withPlugins = require('next-compose-plugins');
-// const withNextBoost = require('next-boost');
-// const withImages = require('next-images');
+const withPWA = require('next-pwa');
 
-// module.exports = withPlugins([
-//   [withNextBoost],
-//   [withImages],
-// ], {
-//   reactStrictMode: true,
-//   // Otras configuraciones de Next.js aquí...
-// });
-
-
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
-}
-
-module.exports = nextConfig
+module.exports = withPWA({
+ 
+});
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,28 @@
+import React, { useEffect } from "react";
 import NextNprogress from "nextjs-progressbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../scss/global.scss";
 
 export default function App(props) {
   const { Component, pageProps } = props;
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "ServiceWorker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("ServiceWorker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
 
   return (
     <>
